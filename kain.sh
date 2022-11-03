9f6bf5048cb023dcb257b03c979df0bd6ef3fbc8
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-11-03 19:34:55 -0300

    Include support for proton.

--- kain.sh
+++ kain.sh
@@ -5,7 +5,7 @@ echo -e "  |      < \__  \ |  |/    \ "
 echo -e "  |    |  \ / __ \|  |   |  \ "
 echo -e "  |____|__ (____  /__|___|  /"
 echo -e "          \/    \/        \/"
-echo -e "  v1.2            eml parser"
+echo -e "  v1.3            eml parser"
 
 if [ "$2" == "" ]
 then
@@ -93,8 +93,7 @@ if [ "$1" == "zimbra" ]
 then
   from=$(grep "From: " $2 | cut -d " " -f 2 | grep -E -o "\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+.[a-zA-Z0-9.-]+\b")
   echo -e "\e[32m[\e[0mFROM\e[32m]\e[0m\t\t\t$from"
-  replyto=$(grep "From: " $2 | cut -d " " -f 3 | cut -d "<" -f 2 | cut -d ">" -f 1
-)
+  replyto=$(grep "From: " $2 | cut -d " " -f 3 | cut -d "<" -f 2 | cut -d ">" -f 1)
   echo -e "\e[32m[\e[0mRP\e[32m]\e[0m\t\t$replyto"
   to=$(grep "To: " $2 | head -n 1 | grep -E -o "\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+.[a-zA-Z0-9.-]+\b")
   echo -e "\e[32m[\e[0mTO\e[32m]\e[0m\t\t\t$to"
@@ -114,7 +113,43 @@ then
   else
     echo -e "\e[31m[\e[0mDMARC\e[31m]\t\t\t\e[31m$dmarc\e[0m"
   fi
-  dkim=$(grep dkim= $2 | cut -d " " -f 1 | cut -d "=" -f 2)
+  dkim=$(grep "dkim=" $2 | cut -d " " -f 1 | cut -d "=" -f 2)
+  if [ "$dkim" == "pass" ]
+  then
+    echo -e "\e[32m[\e[0mDKIM\e[32m]\t\t\t$dkim"
+  else
+    echo -e "\e[31m[\e[0mDKIM\e[31m]\t\t\t\e[31m$dkim\e[0m"
+  fi
+fi
+
+# -------------------- #
+# ProtonMail
+# -------------------- #
+if [ "$1" == "proton" ]
+then
+  from=$(grep "From: " $2 | cut -d " " -f 2 | grep -E -o "\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+.[a-zA-Z0-9.-]+\b")
+  echo -e "\e[32m[\e[0mFROM\e[32m]\e[0m\t\t\t$from"
+  replyto=$(grep "From: " $2 | cut -d " " -f 2)
+  echo -e "\e[32m[\e[0mRP\e[32m]\e[0m\t\t$replyto"
+  to=$(grep "for <" $2 | cut -d ";" -f 1 | head -n 1 | grep -E -o "\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+.[a-zA-Z0-9.-]+\b")
+  echo -e "\e[32m[\e[0mTO\e[32m]\e[0m\t\t\t$to"
+  source_ip=$(grep "Received: from" $2 | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort | tail -n 1)
+  echo -e "\e[32m[\e[0mIP\e[32m]\e[0m\t\t\t$source_ip"
+  spf=$(grep "spf=pass" $2 | cut -d " " -f 3 | cut -d "=" -f 2)
+  if [ "$spf" == "pass" ]
+  then
+    echo -e "\e[32m[\e[0mSPF\e[32m]\t\t\t$spf"
+  else
+    echo -e "\e[31m[\e[0mSPF\e[31m]\t\t\e[31m$spf\e[0m"
+  fi
+  dmarc=$(grep "dmarc" $2 | cut -d " " -f 3 | cut -d "=" -f 2)
+  if [ "$dmarc" == "pass" ]
+  then
+    echo -e "\e[32m[\e[0mDMARC\e[32m]\t\t\t$dmarc"
+  else
+    echo -e "\e[31m[\e[0mDMARC\e[31m]\t\t\t\e[31m$dmarc\e[0m"
+  fi
+  dkim=$(grep "dkim" $2 | head -n 1 | cut -d " " -f 3 | cut -d "=" -f 2)
   if [ "$dkim" == "pass" ]
   then
     echo -e "\e[32m[\e[0mDKIM\e[32m]\t\t\t$dkim"
